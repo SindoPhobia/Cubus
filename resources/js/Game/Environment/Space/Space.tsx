adeed55f62a15546ab687627035119350a29388f
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-14 10:48:56 +0200

    [feat-environment-design] add nebula clouds to space

--- resources/js/Game/Environment/Space/Space.tsx
+++ resources/js/Game/Environment/Space/Space.tsx
@@ -1,12 +1,14 @@
 import {memo} from 'react';
 import {Stars} from './Stars';
 import {Planets} from './Planets';
+import { Nebula } from './Nebula';
 
 export const Space = memo(() => {
     return (
         <>
             <Stars />
             <Planets />
+            <Nebula />
         </>
     );
 });
