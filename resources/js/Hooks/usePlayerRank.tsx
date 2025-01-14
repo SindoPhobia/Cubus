f8039d58dec3ad0370dcb4b7ceaefc4c9cc3b314
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-14 12:25:30 +0200

    adjust player rank hook to support move's points update

--- resources/js/Hooks/usePlayerRank.tsx
+++ resources/js/Hooks/usePlayerRank.tsx
@@ -1,12 +1,24 @@
 import {COLORS} from '@/Constants/colors';
 import {useAppState} from '@/Store/app_state';
 import {useBoardState} from '@/Store/board_state';
+import { useUserEventsStore } from '@/Store/user_events_store';
 import {PlayerColor} from '@/types/game';
+import { GameSession } from '@/types/models/tables/Session';
 import {useMemo} from 'react';
 
 export const usePlayerRank = () => {
-    const session = useAppState(s => s.currentSession);
+    const currentSession = useAppState(s => s.currentSession);
+    const latestMove = useUserEventsStore(s => s.latestMove);
     const playerState = useBoardState(s => s.playerState);
+    const session = latestMove?.session ?? currentSession;
+
+    const points = useMemo(() => {
+        if(session && playerState){
+            return session[`player_${playerState.session_color}_points` as keyof GameSession] as number;
+        }else{
+            return 0;
+        }
+    }, [session, playerState]);
 
     const position = useMemo(() => {
         const playerColor = playerState?.session_color;
@@ -25,6 +37,7 @@ export const usePlayerRank = () => {
         });
         return playerPosition;
     }, [session, playerState]);
+
     return {
         rank: position,
         isWin: position === 1,
@@ -36,5 +49,6 @@ export const usePlayerRank = () => {
                   : position === 3
                     ? '3rd'
                     : '4th',
+        playerPoints: points,
     };
 };
