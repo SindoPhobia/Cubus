0fc9b7a0ffe140e9f9eb839f0265707c963bc216
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-15 19:14:07 +0200

    [feat-performance] add performance metric to game settings

--- resources/js/Store/game_settings.ts
+++ resources/js/Store/game_settings.ts
@@ -3,16 +3,20 @@ import {create} from 'zustand';
 export type GameSettings = {
     gridHelper: boolean;
     enableLights: boolean;
+    enablePerf: boolean;
 };
 
 type Actions = {
     setGridHelper: (gridHelper: boolean) => void;
     setEnableLights: (enableLights: boolean) => void;
+    setEnablePerf: (enablePerf: boolean) => void;
 };
 
 export const useGameSettings = create<GameSettings & Actions>((set, get) => ({
     gridHelper: false,
     enableLights: true,
+    enablePerf: false,
     setGridHelper: gridHelper => set({gridHelper}),
     setEnableLights: enableLights => set({enableLights}),
+    setEnablePerf: enablePerf => set({enablePerf}),
 }));
